d9ce0c917034c180b184421f3605fe8720b5279d
Oscar Maciel C <oscarmacielc@live.com.mx>
2025-06-13 01:57:42 -0600

    cambio a singular unidad administrativa

--- database/migrations/2025_06_12_001022_create_gerencias_table.php
+++ database/migrations/2025_06_12_001022_create_gerencias_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('gerencias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre')->unique();
-            $table->foreignId('unidad_administrativa_id')->constrained()->onDelete('cascade');
+            $table->foreignId('unidad_administrativa_id')->constrained('unidades_administrativas')->onDelete('cascade');
             $table->foreignId('user_id')
                 ->nullable()
                 ->unique()
