d051b5ff25ce45ee924dc7fd8ab88e037629f955
Oscar Maciel C <oscarmacielc@live.com.mx>
2025-06-14 17:50:49 -0600

    Eliminación de importaciones sin uso.

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 use Spatie\Permission\Models\Role;
 
 
